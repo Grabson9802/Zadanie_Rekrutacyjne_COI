1820d57fcae35266bbc664f20819c6d712c886a0
Krystian Grabowy <krys9802@gmail.com>
2024-02-16 11:21:29 +0100

    Initial Commit

---
+++ Zadanie_Rekrutacyjne_COI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zadanie_Rekrutacyjne_COI
+//
+//  Created by Krystian Grabowy on 16/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zadanie_Rekrutacyjne_COI/Zadanie_Rekrutacyjne_COIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zadanie_Rekrutacyjne_COIApp.swift
+//  Zadanie_Rekrutacyjne_COI
+//
+//  Created by Krystian Grabowy on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Zadanie_Rekrutacyjne_COIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
